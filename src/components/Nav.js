a7824473778ca2ce59a2630bf2925fb6e893ef13
shan5742 <asamshan456@gmail.com>
2018-11-21 10:31:46 +0000

    styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,22 +4,22 @@ class Nav extends Component {
   render() {
     return (
       <div className="Nav tc">
-        <header className="bg-light-blue w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+        <header className="bg-black w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
           <nav className="f4 fw6 ttu tracked">
-            <a className="link dim black dib mr3" href="/" title="Home">
+            <a className="link dim white dib mr3" href="/" title="Home">
               Home
             </a>
             <a
-              className="link dim black dib mr3"
+              className="link dim white dib mr3"
               href="/signup"
               title="Sign Up"
             >
               Sign Up
             </a>
-            <a className="link dim black dib mr3" href="/login" title="Log In">
+            <a className="link dim white dib mr3" href="/login" title="Log In">
               Log In
             </a>
-            <a className="link dim black dib" href="/about" title="About">
+            <a className="link dim white dib" href="/about" title="About">
               About
             </a>
           </nav>
