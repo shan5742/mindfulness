1852ff8ad6aaf21796105d90c12fe02b49d104f2
shan5742 <asamshan456@gmail.com>
2018-11-21 11:18:24 +0000

    styling form eelements

--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -20,44 +20,51 @@ class LogIn extends Component {
       this.state.email === "" ||
       this.state.name === "";
     return (
-      <div className="tc">
-        <h1 className="f1">100 Days of Mindfulness</h1>
-        <h2 className="f2">Log In</h2>
-        <form>
-          <input
-            name="name"
-            value={this.state.name}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Full Name"
-          />
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Email Address"
-          />
-          <input
-            name="passwordOne"
-            value={this.state.passwordOne}
-            onChange={this.onChange}
-            type="password"
-            placeholder="Password"
-          />
-          <input
-            name="passwordTwo"
-            value={this.state.passwordTwo}
-            onChange={this.onChange}
-            type="password"
-            placeholder="Confirm Password"
-          />
-          <button disabled={isInvalid} type="submit">
-            Sign Up
-          </button>
-
-          {this.state.error && <p>{this.state.error.message}</p>}
-        </form>
+      <div>
+        <h1 className="f1 tc">Log In</h1>
+        <article class="pa4 black-80">
+          <p className="f4">
+            Please enter your email address and password below to log in
+          </p>
+          <form>
+            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+              <legend class="ph0 mh0 fw6 clip">Sign Up</legend>
+              <div class="mt3">
+                <label class="db fw4 lh-copy f6" for="name">
+                  Email Adrress
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="name"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  type="text"
+                  placeholder="Please enter your email"
+                />
+              </div>
+              <div class="mt3">
+                <label class="db fw4 lh-copy f6" for="password">
+                  Password
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="passwordOne"
+                  value={this.state.passwordOne}
+                  onChange={this.onChange}
+                  type="password"
+                  placeholder="Please enter your Password"
+                />
+              </div>
+            </fieldset>
+            <div class="mt3">
+              <input
+                class="b ph3 pv2 input-reset ba b--black bg-black white grow pointer f6"
+                type="submit"
+                value="Log In"
+              />
+            </div>
+          </form>
+        </article>
       </div>
     );
   }
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -20,44 +20,78 @@ class SignUp extends Component {
       this.state.email === "" ||
       this.state.name === "";
     return (
-      <div className="tc">
-        <h1 className="f1">100 Days of Mindfulness</h1>
-        <h2 className="f2">Sign Up</h2>
-        <form>
-          <input
-            name="name"
-            value={this.state.name}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Full Name"
-          />
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Email Address"
-          />
-          <input
-            name="passwordOne"
-            value={this.state.passwordOne}
-            onChange={this.onChange}
-            type="password"
-            placeholder="Password"
-          />
-          <input
-            name="passwordTwo"
-            value={this.state.passwordTwo}
-            onChange={this.onChange}
-            type="password"
-            placeholder="Confirm Password"
-          />
-          <button disabled={isInvalid} type="submit">
-            Sign Up
-          </button>
-
-          {this.state.error && <p>{this.state.error.message}</p>}
-        </form>
+      <div>
+        <h1 className="f1 tc">Sign Up for 100 Days of Mindfulness</h1>
+        <article class="pa4 black-80">
+          <p className="f4">
+            Please fill in the details below to get started with 100 Days of
+            Mindfulness
+          </p>
+          <form>
+            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+              <legend class="ph0 mh0 fw6 clip">Sign Up</legend>
+              <div class="mt3">
+                <label class="db fw6 lh-copy f6" for="name">
+                  Name
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                  type="text"
+                  placeholder="Please enter your full name"
+                />
+              </div>
+              <div class="mt3">
+                <label class="db fw6 lh-copy f6" for="name">
+                  Email Address
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="name"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  type="text"
+                  placeholder="Please enter a valid email address"
+                />
+              </div>
+              <div class="mt3">
+                <label class="db fw6 lh-copy f6" for="password">
+                  Password
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="passwordOne"
+                  value={this.state.passwordOne}
+                  onChange={this.onChange}
+                  type="password"
+                  placeholder="Please enter a strong Password"
+                />
+              </div>
+              <div class="mt3">
+                <label class="db fw6 lh-copy f6" for="password">
+                  Confirm Password
+                </label>
+                <input
+                  class="pa2 input-reset ba bg-white w-100 measure"
+                  name="passwordTwo"
+                  value={this.state.passwordTwo}
+                  onChange={this.onChange}
+                  type="password"
+                  placeholder="Confirm your Password"
+                />
+              </div>
+            </fieldset>
+            <div class="mt3">
+              <input
+                class="b ph3 pv2 input-reset ba b--black bg-black white grow pointer f6"
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </form>
+        </article>
       </div>
     );
   }
