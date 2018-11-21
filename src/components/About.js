06e2e577ec8b01b57a2947773fc7f3891542927e
shan5742 <asamshan456@gmail.com>
2018-11-21 15:22:59 +0000

    adding styling and crisis info

--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,20 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1 className="tc f1 black">What's all This?</h1>
-        <h2 className="tc f3 black">About 100 Days of Mindfulness</h2>
-        <p className="tc white">
+        <h2 className="tc f1 black">About 100 Days of Mindfulness</h2>
+        <p className="tc white pa2">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
           omnis cum, fugit, quas ad porro impedit a est illum saepe itaque
           recusandae laborum accusantium distinctio possimus rem! Quos, tempora
           ad.
         </p>
-        <p className="tc white">
+        <p className="tc white pa2">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
           omnis cum, fugit, quas ad porro impedit a est illum saepe itaque
           recusandae laborum accusantium distinctio possimus rem! Quos, tempora
           ad.
         </p>
-        <p className="tc white mb7">
+        <p className="tc white pa2 mb6">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
           omnis cum, fugit, quas ad porro impedit a est illum saepe itaque
           recusandae laborum accusantium distinctio possimus rem! Quos, tempora
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer class="pv4 ph3 ph5-m ph6-l mid-gray tc" style={{ flexShrink: 0 }}>
-        <p className="black f4 b">Made with love</p>
+      <footer class="pv2 ph3 ph5-m ph6-l mid-gray tc" style={{ flexShrink: 0 }}>
+        <p className="black f4 b">Made with love & React by Asam</p>
       </footer>
     );
   }
